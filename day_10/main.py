1530f39857b169789c107d3f02331866c9fcca37
Endyya <endya@protonmail.com>
2024-12-05 16:12:13 +0100

    Day 10 * Finished

---
+++ day_10/main.py
@@ -0,0 +1,25 @@
+import re
+
+
+
+def next_chunk(chunk):
+    assert len(set(chunk)) == 1
+    return f'{len(chunk)}{chunk[0]}'
+
+def next_line(line):
+    same_digit_regex = '1+|2+|3+'
+    pattern = re.compile(same_digit_regex)
+    to_return = ''
+    for chunk in pattern.findall(line):
+        to_return += next_chunk(chunk)
+    return to_return
+        
+    
+line = '3113322113'
+for _ in range(40):
+    line = next_line(line)
+
+print('part 1 :', len(line))
+
+
+    
